60b5c4dfa96e8683a32948eb654c2fdfd00e3fd4
Edgar Duran <edgarduran@Edgars-MacBook-Pro-8.local>
2016-01-20 11:49:40 -0700

    link works and emails

--- app/controllers/mailers_controller.rb
+++ app/controllers/mailers_controller.rb
@@ -2,7 +2,7 @@ require 'sendgrid-ruby'
 class MailersController < ApplicationController
 
   def create
-    mail_service.send_email(params)
+    mail_service.send_email(params, request.HTTP_ORIGIN)
     flash[:success] = "Email Sent"
     redirect_to playlists_path
   end
--- app/services/mail_service.rb
+++ app/services/mail_service.rb
@@ -6,14 +6,21 @@ class MailService
     @client = SendGrid::Client.new(api_key: ENV['YOUR_SENDGRID_APIKEY'])
   end
 
-  def send_email(params)
+  def send_email(params, origin)
     mail = SendGrid::Mail.new do |m|
       m.to = params[:email]
       m.from = 'taco@cat.limo'
       m.subject = 'Contribute to my Playlist'
-      m.text = 'I heard you like pineapple.'
+      m.text = "#{origin}/requests/#{path_name(params)}"
     end
     client.send(mail)
+  end
+
+  private
+
+  def path_name(params)
+    spotify_id = params[:rspotify].sub("#", "%23")
+    "#{spotify_id}?pl_id=#{params[:pl_id]}&pl_name=#{params[:pl_name]}&user=#{params[:user]}&user_id=#{params[:user_id]}"
   end
 
 
