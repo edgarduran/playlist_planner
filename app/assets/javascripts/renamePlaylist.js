ce8b23100acdfc3b34ad7fcaa39ff7509aabadda
Edgar Duran <edgar.eduran@gmail.com>
2016-08-29 11:15:56 -0600

    fix rename multi-call

--- app/assets/javascripts/renamePlaylist.js
+++ app/assets/javascripts/renamePlaylist.js
@@ -1,6 +1,9 @@
 function renamePlaylist() {
   $('.rename-btn').on('click', function(evt) {
     evt.preventDefault();
+    var that = $(this);
+    that.off('click');
+
     var newName = $('#name').val();
     if (newName === "") {
       alert("Name can not be blank");
@@ -14,12 +17,15 @@ function renamePlaylist() {
               name: newName,
             },
       success: function() {
-        $('#title').text(newName + 'PLP');
+        $('#title').text(newName + ' PLP');
         $('#rename-playlist').closeModal();
+        $('#name').val('');
         Materialize.toast('Playlist name changed to ' + newName, 5000);
       },
       error: function(xhr) {
         console.log(xhr.responseText);
+        $('#rename-playlist').closeModal();
+        $('#name').val('');
         Materialize.toast('Oops! Somthing went wrong. Please try again', 5000);
       }
     });
